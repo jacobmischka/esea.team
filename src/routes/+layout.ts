ef0e9fc5dee0291a69e3d03c986f5714e7842130
Jacob Mischka <jacob@mischka.me>
2024-12-03 00:47:51 -0600

    Add basic analytics, just curious about usage

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,7 @@
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+import { dev } from '$app/environment';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
+
 export const ssr = false;
